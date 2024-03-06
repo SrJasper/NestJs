cddab66be14eb41c4437ed7d87e652cd5082ce51
josuerx12 <josueaze12@gmail.com>
2024-03-06 18:48:38 -0300

    core: variaveis de ambiente adicionadas

--- app.js
+++ app.js
@@ -1,29 +1,39 @@
-const express = require ('express');
-const { engine } = require('express-handlebars');
-const fileupload = require('express-fileupload');
+require("dotenv").config();
+const express = require("express");
+const { engine } = require("express-handlebars");
+const fileupload = require("express-fileupload");
 const app = express();
-const rota_produto = require('./rotas/produtos_rota');
+const rota_produto = require("./rotas/produtos_rota");
 
 app.use(fileupload());
-app.use('/bootstrap', express.static('./node_modules/bootstrap/dist'));
+app.use("/bootstrap", express.static("./node_modules/bootstrap/dist"));
 
 // Configuração do express-handlebars
-app.engine('handlebars', engine({
+app.engine(
+  "handlebars",
+  engine({
     helpers: {
       // Função auxiliar para verificar igualdade
       condicionalIgualdade: function (parametro1, parametro2, options) {
-        return parametro1 === parametro2 ? options.fn(this) : options.inverse(this);
-      }
-    }
-  }));
-app.set('view engine', 'handlebars');
-app.set('views', './views');
-app.use('/css', express.static('./css'));
-app.use('/imagens', express.static("./imagens"))
+        return parametro1 === parametro2
+          ? options.fn(this)
+          : options.inverse(this);
+      },
+    },
+  })
+);
+app.set("view engine", "handlebars");
+app.set("views", "./views");
+app.use("/css", express.static("./css"));
+app.use("/imagens", express.static("./imagens"));
 
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
-app.use('/', rota_produto);
+app.use("/", rota_produto);
 
-app.listen(8080);
+app.listen(process.env.PORT, () =>
+  console.log(
+    `Servidor rodando localmente em http://localhost:${process.env.PORT}`
+  )
+);
