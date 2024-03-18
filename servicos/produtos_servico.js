ae989fe96413b5f15cb3a8a29a6933cbcdd4d79e
SrJasper <lulevita1@hotmail.com>
2024-03-18 13:20:57 -0300

    final-commit

--- servicos/produtos_servico.js
+++ servicos/produtos_servico.js
@@ -1,45 +1,45 @@
 // Importar o módulo de conexão com banco
 const Product = require("../models/product")
 
-const fs = require('fs');
+const fs = require('fs')
 const path = require("path")
 
 function formularioCadastro(req, res){
-    res.render('formulario');
+    res.render('formulario')
 }
 
 // Função para realizar o cadastro de produtos
 async function cadastrarProduto(req, res) {
     try {
-        let nome = req.body.nome;
-        let valor = req.body.valor;
-        let imagem = req.files.imagem.name;
-        let categoria = req.body.categoria;
+        let nome = req.body.nome
+        let valor = req.body.valor
+        let imagem = req.files.imagem.name
+        let categoria = req.body.categoria
 
         if (nome == '' || valor == '' || isNaN(valor) || categoria == '') {
-            return res.redirect('/falhaCadastro');
+            return res.redirect('/falhaCadastro')
         }
 
-        req.files.imagem.mv(path.join(__dirname,  "..", '/imagens/', req.files.imagem.name));
+        req.files.imagem.mv(path.join(__dirname,  "..", '/imagens/', req.files.imagem.name))
        
         await Product.create({nome, value: valor, img: imagem, category: categoria})
-        return res.redirect('/okCadastro');
+        return res.redirect('/okCadastro')
     } catch(erro) {
-        console.log("Erro durante o cadastro do produto:", erro);
-        res.redirect('/falhaCadastro');
+        console.log("Erro durante o cadastro do produto:", erro)
+        return res.redirect('/falhaCadastro')
     }
 }
 
 // Função para exibir o formulário para cadastro de produtos e a situação
 function formularioCadastroComSituacao(req, res){
-    res.render('formulario', {situacao:req.params.situacao});
+    res.render('formulario', {situacao:req.params.situacao})
 }
 
 // Função para exibir o formulário para edição de produtos
 async function formularioEditar(req, res){
     try {
         const produto = await Product.findById(req.params.id)
-        res.render('formularioEditar', {produto});
+        res.render('formularioEditar', {produto})
     } catch (error) {
         console.log("Erro ao cadastrar")
     }
@@ -47,7 +47,7 @@ async function formularioEditar(req, res){
 // FEITO
 // Função para exibir a listagem de produtos
 async function listagemProdutos(req, res){
-    const categoria = req.params.categoria;
+    const categoria = req.params.categoria
     try {
         if(categoria === 'todos'){
             const produtos = await Product.find()
@@ -70,7 +70,7 @@ async function pesquisa(req, res){
         let semRegistros = false  
         const produto = await Product.find({ nome: { $regex: req.body.termo, $options: 'i' } })
         if(!produto || produto.length <= 0) semRegistros = true
-        return res.render('lista', {produtos: produto, semRegistros:semRegistros});
+        return res.render('lista', {produtos: produto, semRegistros:semRegistros})
         
     } catch (error) {
         return res.render("err")
@@ -89,10 +89,10 @@ async function removerProduto(req, res){
 
         await produto.deleteOne()
         console.log("O produto foi deletado")
-        res.redirect('/okRemover');
+        res.redirect('/okRemover')
     } catch (error) {
         console.log("Não foi possivel deletar o item")
-        res.redirect('/falhaRemover');
+        res.redirect('/falhaRemover')
     }
 }
 //FEITA
@@ -113,15 +113,15 @@ async function editarProduto(req, res){
             if(newImg) {
                 fs.unlink(path.join(__dirname, "..", "/imagens/", product.img), (err) => {
                     if (err) {
-                        console.error('Erro ao excluir a imagem:', err);
+                        console.error('Erro ao excluir a imagem:', err)
                     }
-                });
-                newImg.mv(path.join(__dirname, "..",'/imagens/',newImg.name)); 
+                })
+                newImg.mv(path.join(__dirname, "..",'/imagens/',newImg.name)) 
             }
 
             await product.updateOne({ nome, value: valor, img: newImg ? newImg.name : defaultImg })
 
-            return  res.redirect('/okEdicao');   
+            return  res.redirect('/okEdicao')   
         } catch (error) {
             console.log(error)     
             return res.redirect('/falhaEdicao')    
@@ -139,4 +139,4 @@ module.exports = {
     cadastrarProduto,
     removerProduto,
     editarProduto
-};
+}
